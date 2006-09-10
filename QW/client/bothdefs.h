628747f95ec27055a151aa044a225a818742fdec
Tyrann <tyrann@disenchant.net>
2006-09-10 19:13:51 +0930

    [PATCH] Define bitfields using shifts
    To make the item bitfields a little more readable, define them using
    shifts. Tidy up the spacing (tab vs. space) of these particular defines.
    
    Signed-off-by: Tyrann <tyrann@disenchant.net>

--- QW/client/bothdefs.h
+++ QW/client/bothdefs.h
@@ -90,73 +90,64 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 // stats are integers communicated to the client by the server
 //
-#define	MAX_CL_STATS		32
-#define	STAT_HEALTH			0
-//define        STAT_FRAGS                      1
-#define	STAT_WEAPON			2
-#define	STAT_AMMO			3
-#define	STAT_ARMOR			4
-//define        STAT_WEAPONFRAME        5
-#define	STAT_SHELLS			6
-#define	STAT_NAILS			7
-#define	STAT_ROCKETS		8
-#define	STAT_CELLS			9
-#define	STAT_ACTIVEWEAPON	10
-#define	STAT_TOTALSECRETS	11
-#define	STAT_TOTALMONSTERS	12
-#define	STAT_SECRETS		13	// bumped on client side by svc_foundsecret
-#define	STAT_MONSTERS		14	// bumped by svc_killedmonster
-#define	STAT_ITEMS			15
-//define        STAT_VIEWHEIGHT         16
+#define MAX_CL_STATS            32
+#define STAT_HEALTH             0
+//#define STAT_FRAGS              1
+#define STAT_WEAPON             2
+#define STAT_AMMO               3
+#define STAT_ARMOR              4
+#define STAT_WEAPONFRAME        5
+#define STAT_SHELLS             6
+#define STAT_NAILS              7
+#define STAT_ROCKETS            8
+#define STAT_CELLS              9
+#define STAT_ACTIVEWEAPON       10
+#define STAT_TOTALSECRETS       11
+#define STAT_TOTALMONSTERS      12
+#define STAT_SECRETS            13 /* bumped client side by svc_foundsecret */
+#define STAT_MONSTERS           14 /* bumped by svc_killedmonster */
+#define STAT_ITEMS              15
+//#define STAT_VIEWHEIGHT         16
 
 
 //
 // item flags
 //
-#define	IT_SHOTGUN				1
-#define	IT_SUPER_SHOTGUN		2
-#define	IT_NAILGUN				4
-#define	IT_SUPER_NAILGUN		8
+#define IT_SHOTGUN              (1 << 0)
+#define IT_SUPER_SHOTGUN        (1 << 1)
+#define IT_NAILGUN              (1 << 2)
+#define IT_SUPER_NAILGUN        (1 << 3)
+#define IT_GRENADE_LAUNCHER     (1 << 4)
+#define IT_ROCKET_LAUNCHER      (1 << 5)
+#define IT_LIGHTNING            (1 << 6)
+#define IT_SUPER_LIGHTNING      (1 << 7)
+#define IT_SHELLS               (1 << 8)
+#define IT_NAILS                (1 << 9)
+#define IT_ROCKETS              (1 << 10)
+#define IT_CELLS                (1 << 11)
+#define IT_AXE                  (1 << 12)
+#define IT_ARMOR1               (1 << 13)
+#define IT_ARMOR2               (1 << 14)
+#define IT_ARMOR3               (1 << 15)
+#define IT_SUPERHEALTH          (1 << 16)
+#define IT_KEY1                 (1 << 17)
+#define IT_KEY2                 (1 << 18)
+#define IT_INVISIBILITY         (1 << 19)
+#define IT_INVULNERABILITY      (1 << 20)
+#define IT_SUIT                 (1 << 21)
+#define IT_QUAD                 (1 << 22)
 
-#define	IT_GRENADE_LAUNCHER		16
-#define	IT_ROCKET_LAUNCHER		32
-#define	IT_LIGHTNING			64
-#define	IT_SUPER_LIGHTNING		128
-
-#define	IT_SHELLS				256
-#define	IT_NAILS				512
-#define	IT_ROCKETS				1024
-#define	IT_CELLS				2048
-
-#define	IT_AXE					4096
-
-#define	IT_ARMOR1				8192
-#define	IT_ARMOR2				16384
-#define	IT_ARMOR3				32768
-
-#define	IT_SUPERHEALTH			65536
-
-#define	IT_KEY1					131072
-#define	IT_KEY2					262144
-
-#define	IT_INVISIBILITY			524288
-
-#define	IT_INVULNERABILITY		1048576
-#define	IT_SUIT					2097152
-#define	IT_QUAD					4194304
-
-#define	IT_SIGIL1				(1<<28)
-
-#define	IT_SIGIL2				(1<<29)
-#define	IT_SIGIL3				(1<<30)
-#define	IT_SIGIL4				(1<<31)
+#define IT_SIGIL1               (1 << 28)
+#define IT_SIGIL2               (1 << 29)
+#define IT_SIGIL3               (1 << 30)
+#define IT_SIGIL4               (1 << 31)
 
 //
 // print flags
 //
-#define	PRINT_LOW			0	// pickup messages
-#define	PRINT_MEDIUM		1	// death messages
-#define	PRINT_HIGH			2	// critical messages
-#define	PRINT_CHAT			3	// chat messages
+#define PRINT_LOW               0 /* pickup messages */
+#define PRINT_MEDIUM            1 /* death messages */
+#define PRINT_HIGH              2 /* critical messages */
+#define PRINT_CHAT              3 /* chat messages */
 
 #endif /* CLIENT_BOTHDEFS_H */
