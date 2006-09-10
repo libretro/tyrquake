16fb5dd77c3394f70ba92ff651fd06b40e256f4b
Tyrann <tyrann@disenchant.net>
2006-09-10 19:12:22 +0930

    [PATCH] Remove obsoleted Cvar_CompleteVariable
    This hasn't been used for a long time. Remove it.
    
    Signed-off-by: Tyrann <tyrann@disenchant.net>

--- include/cvar.h
+++ include/cvar.h
@@ -122,13 +122,6 @@ float Cvar_VariableValue(char *var_name);
 char *Cvar_VariableString(const char *var_name);
 
 /*
- * attempts to match a partial variable name for command line completion
- * returns NULL if nothing fits
- * FIXME - use common completion functions (shell.h)
- */
-char *Cvar_CompleteVariable(char *partial);
-
-/*
  * called by Cmd_ExecuteString when Cmd_Argv(0) doesn't match a known command.
  * Returns true if the command was a variable reference that was
  * handled. (print or change)
