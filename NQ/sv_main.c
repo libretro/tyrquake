0947565f7fb874a65b47ebf2747a2ed372b0e86b
Kevin Shanahan <kmshanah@disenchant.net>
2012-10-07 10:44:37 +1030

    Fix off-by-one in localmodels array size calculation
    Introduced in 8088798. Oops!
    
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- NQ/sv_main.c
+++ NQ/sv_main.c
@@ -42,7 +42,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 server_t sv;
 server_static_t svs;
 
-#define MODSTRLEN (sizeof(stringify(MAX_MODELS)) / sizeof(char))
+#define MODSTRLEN (sizeof("*" stringify(MAX_MODELS)) / sizeof(char))
 char localmodels[MAX_MODELS][MODSTRLEN]; // inline model names for precache
 
 //============================================================================
