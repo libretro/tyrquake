deb4578bc5e85b841076870deadb33059b157c04
Kevin Shanahan <kmshanah@disenchant.net>
2012-10-21 17:24:10 +1030

    efrags: remove unneeded include
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- common/gl_refrag.c
+++ common/gl_refrag.c
@@ -25,10 +25,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "sys.h"
 
-#ifdef QW_HACK
-#include "client.h"
-#endif
-
 mnode_t *r_pefragtopnode;
 
 
