6086ccbf404c62d5b7ca3b5d3048ac3c3d1b0b1f
Kevin Shanahan <kmshanah@disenchant.net>
2012-11-12 08:27:41 +1030

    progs: rename the q1 (id1) progs header
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- NQ/progdefs.h
+++ NQ/progdefs.h
@@ -22,6 +22,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define PROGDEFS_H
 
 #include "mathlib.h"
-#include "progdefs.q1"
+#include "progdefs-id1.h"
 
 #endif /* PROGDEFS_H */
