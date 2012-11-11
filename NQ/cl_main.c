libretro/tyrquake

commit 293084be9b455c025bdd3565738470efea2fff28

nq: handle visedicts similarly to qw
Make handling of cl_visedicts as similar as possible to qw. I am not yet
100% conviced that this has not introduced subtle bugs due to now taking a
copy of the entities in this list, but merging the behaviour is probably
the right way to go (and fix the bugs later).

Quickly played through a couple of random maps and nothing jumped out as
being obviously incorrect.

Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

NQ/cl_main.c
@@ -70,7 +70,8 @@ lightstyle_t cl_lightstyle[MAX_LIGHTSTYLES];
 dlight_t cl_dlights[MAX_DLIGHTS];
 
 int cl_numvisedicts;
-entity_t *cl_visedicts[MAX_VISEDICTS];
+entity_t *cl_visedicts;
+static entity_t cl_visedicts_list[MAX_VISEDICTS];
 
 /*
 =====================
@@ -479,6 +480,7 @@ CL_RelinkEntities(void)
     frac = CL_LerpPoint();
 
     cl_numvisedicts = 0;
+    cl_visedicts = cl_visedicts_list;
 
 //
 // interpolate player info
@@ -627,7 +629,7 @@ CL_RelinkEntities(void)
 	    continue;
 
 	if (cl_numvisedicts < MAX_VISEDICTS) {
-	    cl_visedicts[cl_numvisedicts] = ent;
+	    cl_visedicts[cl_numvisedicts] = *ent;
 	    cl_numvisedicts++;
 	}
     }

NQ/client.h
@@ -303,7 +303,7 @@ void CL_NextDemo(void);
 
 #define MAX_VISEDICTS 256
 extern int cl_numvisedicts;
-extern entity_t *cl_visedicts[MAX_VISEDICTS];
+extern entity_t *cl_visedicts;
 
 extern int fps_count;
 

common/gl_rmain.c
@@ -674,12 +674,7 @@ R_DrawEntitiesOnList(void)
 
     // draw sprites seperately, because of alpha blending
     for (i = 0; i < cl_numvisedicts; i++) {
-#ifdef NQ_HACK
-	currententity = cl_visedicts[i];
-#endif
-#ifdef QW_HACK
 	currententity = &cl_visedicts[i];
-#endif
 	switch (currententity->model->type) {
 	case mod_alias:
 	    R_DrawAliasModel(currententity);
@@ -693,12 +688,7 @@ R_DrawEntitiesOnList(void)
     }
 
     for (i = 0; i < cl_numvisedicts; i++) {
-#ifdef NQ_HACK
-	currententity = cl_visedicts[i];
-#endif
-#ifdef QW_HACK
 	currententity = &cl_visedicts[i];
-#endif
 	switch (currententity->model->type) {
 	case mod_sprite:
 	    R_DrawSpriteModel(currententity);
@@ -1163,7 +1153,7 @@ R_Mirror(void)
 
     ent = &cl_entities[cl.viewentity];
     if (cl_numvisedicts < MAX_VISEDICTS) {
-	cl_visedicts[cl_numvisedicts] = ent;
+	cl_visedicts[cl_numvisedicts] = *ent;
 	cl_numvisedicts++;
     }
 

NQ/cl_tent.c
@@ -278,7 +278,7 @@ CL_NewTempEntity(void)
     ent = &cl_temp_entities[num_temp_entities];
     memset(ent, 0, sizeof(*ent));
     num_temp_entities++;
-    cl_visedicts[cl_numvisedicts] = ent;
+    cl_visedicts[cl_numvisedicts] = *ent;
     cl_numvisedicts++;
 
     ent->colormap = vid.colormap;

NQ/r_main.c
@@ -564,7 +564,7 @@ R_DrawEntitiesOnList(void)
 	return;
 
     for (i = 0; i < cl_numvisedicts; i++) {
-	currententity = cl_visedicts[i];
+	currententity = &cl_visedicts[i];
 
 	if (currententity == &cl_entities[cl.viewentity])
 	    continue;		// don't draw the player
@@ -768,7 +768,7 @@ R_DrawBEntitiesOnList(void)
     r_dlightframecount = r_framecount;
 
     for (i = 0; i < cl_numvisedicts; i++) {
-	currententity = cl_visedicts[i];
+	currententity = &cl_visedicts[i];
 
 	switch (currententity->model->type) {
 	case mod_brush:

common/gl_refrag.c
@@ -201,28 +201,18 @@ R_StoreEfrags(efrag_t **ppefrag)
     while ((pefrag = *ppefrag) != NULL) {
 	pent = pefrag->entity;
 	clmodel = pent->model;
-
 	switch (clmodel->type) {
 	case mod_alias:
 	case mod_brush:
 	case mod_sprite:
-	    pent = pefrag->entity;
-
 	    if ((pent->visframe != r_framecount) &&
 		(cl_numvisedicts < MAX_VISEDICTS)) {
-#ifdef NQ_HACK
-		cl_visedicts[cl_numvisedicts++] = pent;
-#endif
-#ifdef QW_HACK
-		cl_visedicts[cl_numvisedicts++] = *pent;
-#endif
-		// mark that we've recorded this entity for this frame
+		/* mark that we've recorded this entity for this frame */
 		pent->visframe = r_framecount;
+		cl_visedicts[cl_numvisedicts++] = *pent;
 	    }
-
 	    ppefrag = &pefrag->leafnext;
 	    break;
-
 	default:
 	    Sys_Error("%s: Bad entity type %d", __func__, clmodel->type);
 	}

common/r_efrag.c
@@ -238,33 +238,21 @@ R_StoreEfrags(efrag_t **ppefrag)
     model_t *clmodel;
     efrag_t *pefrag;
 
-
     while ((pefrag = *ppefrag) != NULL) {
 	pent = pefrag->entity;
 	clmodel = pent->model;
-
 	switch (clmodel->type) {
 	case mod_alias:
 	case mod_brush:
 	case mod_sprite:
-	    pent = pefrag->entity;
-
 	    if ((pent->visframe != r_framecount) &&
 		(cl_numvisedicts < MAX_VISEDICTS)) {
-#ifdef NQ_HACK
-		cl_visedicts[cl_numvisedicts++] = pent;
-#endif
-#ifdef QW_HACK
-		cl_visedicts[cl_numvisedicts++] = *pent;
-#endif
-
-		// mark that we've recorded this entity for this frame
+		/* mark that we've recorded this entity for this frame */
 		pent->visframe = r_framecount;
+		cl_visedicts[cl_numvisedicts++] = *pent;
 	    }
-
 	    ppefrag = &pefrag->leafnext;
 	    break;
-
 	default:
 	    Sys_Error("%s: Bad entity type %d", __func__, clmodel->type);
 	}

commit 74a6903d8e063ce541ecff3e28813246a9900eee

gl: fix player colours in nq multiplayer
This broke with 293084b when I changed the handling of cl_visedicts. Kind
of a hackish workaround for now.

Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

NQ/cl_main.c
@@ -74,6 +74,33 @@ entity_t *cl_visedicts;
 static entity_t cl_visedicts_list[MAX_VISEDICTS];
 
 /*
+ * FIXME - horribly hackish because we don't have a way to tell if the
+ *         entity is a player just by looking at it's properties.
+ *
+ * CL_PlayerEntity()
+ * Returns the player number if the entity is a player, 0 otherwise
+ */
+int
+CL_PlayerEntity(entity_t *e)
+{
+    ptrdiff_t offset;
+    int i;
+
+    /* might be a pointer directly into cl_entities... */
+    offset =  e - cl_entities;
+    if (offset >= 1 && offset <= cl.maxclients)
+	return offset;
+
+    /* ...but if not, try to find a match */
+    for (i = 1; i <= cl.maxclients; i++) {
+	if (!memcmp(e, &cl_entities[i], sizeof(entity_t)))
+	    return i;
+    }
+
+    return 0;
+}
+
+/*
 =====================
 CL_ClearState
 

NQ/client.h
@@ -270,6 +270,12 @@ extern dlight_t cl_dlights[MAX_DLIGHTS];
 extern entity_t cl_temp_entities[MAX_TEMP_ENTITIES];
 extern beam_t cl_beams[MAX_BEAMS];
 
+/*
+ * CL_PlayerEntity()
+ * Returns the player number if the entity is a player, 0 otherwise
+ */
+int CL_PlayerEntity(entity_t *e);
+
 //=============================================================================
 
 //

common/gl_rmain.c
@@ -564,10 +564,8 @@ R_DrawAliasModel(entity_t *e)
     // seperately for the players.  Heads are just uncolored.
 #ifdef NQ_HACK
     if (e->colormap != vid.colormap && !gl_nocolors.value) {
-	i = e - cl_entities;
-	if (i >= 1 && i <= cl.maxclients
-	    /* && !strcmp (e->model->name, "progs/player.mdl") */
-	    )
+	i = CL_PlayerEntity(e);
+	if (i)
 	    GL_Bind(playertextures[i - 1]);
     }
 #endif