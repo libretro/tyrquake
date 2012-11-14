7bbe82dc4cf734ba5a54349ab7e404e5176bc4c4
Kevin Shanahan <kmshanah@disenchant.net>
2012-11-14 16:08:24 +1030

    draw: make local cvars static in d_init.c
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- common/d_init.c
+++ common/d_init.c
@@ -24,9 +24,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define NUM_MIPS	4
 
-cvar_t d_subdiv16 = { "d_subdiv16", "1" };
-cvar_t d_mipcap = { "d_mipcap", "0" };
-cvar_t d_mipscale = { "d_mipscale", "1" };
+static cvar_t d_subdiv16 = { "d_subdiv16", "1" };
+static cvar_t d_mipcap = { "d_mipcap", "0" };
+static cvar_t d_mipscale = { "d_mipscale", "1" };
 
 surfcache_t *d_initial_rover;
 qboolean d_roverwrapped;
