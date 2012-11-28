f0669b281c319297b945f204075c8447405a6c96
Kevin Shanahan <kmshanah@disenchant.net>
2012-11-28 11:12:37 +1030

    cmd: remove duplicate declaration already in cmd.h
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- common/cmd.c
+++ common/cmd.c
@@ -34,7 +34,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "protocol.h"
 #endif
 
-void Cmd_ForwardToServer(void);
 #if defined(QW_HACK) && !defined(SERVERONLY)
 static void Cmd_ForwardToServer_f(void);
 #endif
