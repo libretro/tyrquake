19d399c6f320e6ab5ab4c30454e642a442f71254
svdijk <svdijk@sourceforge.net>
2012-12-10 22:00:45 +0100

    Fix SDL.h includes.
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- common/sdl_common.c
+++ common/sdl_common.c
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "sdl.h"
+#include "SDL.h"
 
 #include "qtypes.h"
 #include "sdl_common.h"
--- include/sdl_common.h
+++ include/sdl_common.h
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "sdl.h"
+#include "SDL.h"
 
 extern SDL_Window *sdl_window;
 
