cb5e04de374848697453678af4397f11615551b9
Kevin Shanahan <kmshanah@disenchant.net>
2012-12-22 14:31:16 +1030

    client: constify CL_EstablishConnection
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- NQ/cl_main.c
+++ NQ/cl_main.c
@@ -193,7 +193,7 @@ Host should be either "local" or a net address to be passed on
 =====================
 */
 void
-CL_EstablishConnection(char *host)
+CL_EstablishConnection(const char *host)
 {
     if (cls.state == ca_dedicated)
 	return;
