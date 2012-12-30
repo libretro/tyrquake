52da9d8dfd61ac3a980e7fd3f7465fc2e26ab3bc
Kevin Shanahan <kmshanah@disenchant.net>
2012-12-30 14:02:19 +1030

    asm: dnode_t struct is not used from ASM
    Signed-off-by: Kevin Shanahan <kmshanah@disenchant.net>

--- include/bspfile.h
+++ include/bspfile.h
@@ -146,8 +146,6 @@ typedef struct {
 #define	CONTENTS_CURRENT_UP	-13
 #define	CONTENTS_CURRENT_DOWN	-14
 
-
-// !!! if this is changed, it must be changed in asm_i386.h too !!!
 typedef struct {
     int32_t planenum;
     int16_t children[2];	// negative numbers are -(leafs+1), not nodes
