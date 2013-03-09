df78308cebbf66ee43c36a8eabdc595e6e541727
twinaphex <autechre1024@hotmail.com>
2013-03-09 01:08:52 +0100

    d_local.h - fix extern declarations

--- include/d_local.h
+++ include/d_local.h
@@ -67,9 +67,8 @@ extern float d_sdivzstepu, d_tdivzstepu, d_zistepu;
 extern float d_sdivzstepv, d_tdivzstepv, d_zistepv;
 extern float d_sdivzorigin, d_tdivzorigin, d_ziorigin;
 
-fixed16_t sadjust, tadjust;
-fixed16_t bbextents, bbextentt;
-
+extern fixed16_t sadjust, tadjust;
+extern fixed16_t bbextents, bbextentt;
 
 void D_DrawSpans8(espan_t *pspans);
 void D_DrawSpans16(espan_t *pspans);
