a1c43def934bb4221713c9a73710f8a94f958557
twinaphex <libretro@gmail.com>
2015-09-09 02:48:41 +0200

    Backport from Makaqu (but modify a little) - simplify COM_CheckRegistered

--- common/common.c
+++ common/common.c
@@ -89,27 +89,6 @@ qboolean standard_quake = true, rogue, hipnotic;
 char gamedirfile[MAX_OSPATH];
 #endif
 
-/* FIXME - remove this and checks like it; no need to be registered... */
-// this graphic needs to be in the pak file to use registered features
-static unsigned short pop[] = {
-    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x6600, 0x0000, 0x0000, 0x0000, 0x6600, 0x0000,
-    0x0000, 0x0066, 0x0000, 0x0000, 0x0000, 0x0000, 0x0067, 0x0000,
-    0x0000, 0x6665, 0x0000, 0x0000, 0x0000, 0x0000, 0x0065, 0x6600,
-    0x0063, 0x6561, 0x0000, 0x0000, 0x0000, 0x0000, 0x0061, 0x6563,
-    0x0064, 0x6561, 0x0000, 0x0000, 0x0000, 0x0000, 0x0061, 0x6564,
-    0x0064, 0x6564, 0x0000, 0x6469, 0x6969, 0x6400, 0x0064, 0x6564,
-    0x0063, 0x6568, 0x6200, 0x0064, 0x6864, 0x0000, 0x6268, 0x6563,
-    0x0000, 0x6567, 0x6963, 0x0064, 0x6764, 0x0063, 0x6967, 0x6500,
-    0x0000, 0x6266, 0x6769, 0x6a68, 0x6768, 0x6a69, 0x6766, 0x6200,
-    0x0000, 0x0062, 0x6566, 0x6666, 0x6666, 0x6666, 0x6562, 0x0000,
-    0x0000, 0x0000, 0x0062, 0x6364, 0x6664, 0x6362, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0062, 0x6662, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0061, 0x6661, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000, 0x6500, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000, 0x6400, 0x0000, 0x0000, 0x0000
-};
-
 /*
 
 All of Quake's data access is through a hierchal file system, but the contents
@@ -1167,42 +1146,21 @@ being registered.
 void
 COM_CheckRegistered(void)
 {
-    FILE *f;
-    unsigned short check[128];
-    int i;
-
-    COM_FOpenFile("gfx/pop.lmp", &f);
-    static_registered = 0;
-
-    if (!f) {
-	Con_Printf("Playing shareware version.\n");
-#ifndef SERVERONLY
-	if (com_modified)
-#ifdef NQ_HACK
-	    Sys_Error("You must have the registered version "
-		      "to use modified games");
-#endif
-#ifdef QW_HACK
-	    Sys_Error("You must have the registered version "
-		      "to play QuakeWorld");
-#endif
-#endif /* SERVERONLY */
-	return;
-    }
-
-    fread(check, 1, sizeof(check), f);
-    fclose(f);
-
-    for (i = 0; i < 128; i++)
-	if (pop[i] != (unsigned short)BigShort(check[i]))
-	    Sys_Error("Corrupted data file.");
+   FILE *f;
 
 #ifdef NQ_HACK
-    Cvar_Set("cmdline", com_cmdline);
+   Cvar_Set("cmdline", com_cmdline);
 #endif
-    Cvar_Set("registered", "1");
-    static_registered = 1;
-    Con_Printf("Playing registered version.\n");
+   COM_FOpenFile("gfx/pop.lmp", &f);
+   if (!f)
+      Con_Printf("Playing shareware version.\n");
+   else
+   {
+      fclose(f);
+      Cvar_Set("registered", "1");
+      static_registered = 1;
+      Con_Printf("Playing registered version.\n");
+   }
 }
 
 
