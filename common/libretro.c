92247dbadf741cc73b7cf287d588106783388e2c
twinaphex <libretro@gmail.com>
2015-09-21 11:16:51 +0200

    Use retro_miscellaneous.h

--- common/libretro.c
+++ common/libretro.c
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "libretro.h"
-#include "thread.h"
+#include <retro_miscellaneous.h>
 
 #if defined(_WIN32) && !defined(_XBOX)
 #include <windows.h>
