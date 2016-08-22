cd2c83e9769bb16c93995e541bf4251f5f1e7240
twinaphex <libretro@gmail.com>
2016-08-22 16:31:25 +0200

    Checked wad.c

--- common/wad.h
+++ common/wad.h
@@ -36,8 +36,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #define	TYP_LUMPY		64	// 64 + grab command number
 #define	TYP_PALETTE		64
-#define	TYP_QTEX		65
-#define	TYP_QPIC		66
+#define	TYP_QTEX		   65
+#define	TYP_QPIC		   66
 #define	TYP_SOUND		67
 #define	TYP_MIPTEX		68
 
