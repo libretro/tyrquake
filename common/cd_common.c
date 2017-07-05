5c01ba2fb9e7a66dea099ef1e828802d32718982
twinaphex <libretro@gmail.com>
2017-07-05 08:42:52 +0200

    Include another header

--- common/cd_common.c
+++ common/cd_common.c
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <string.h>
 #include <time.h>
+#include <boolean.h>
 
 #include "qtypes.h"
 #include "cdaudio.h"
