9142f0c0e2f093e63afe68364eaaaaf758439d34
twinaphex <libretro@gmail.com>
2021-11-02 15:11:11 +0100

    remove stdio includes

--- QW/server/qwsvdef.h
+++ QW/server/qwsvdef.h
@@ -28,7 +28,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <math.h>
 #include <string.h>
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
 #include <ctype.h>
--- common/spritegn.h
+++ common/spritegn.h
@@ -49,7 +49,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifdef INCLUDELIBS
 
 #include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
 #include <string.h>
 
