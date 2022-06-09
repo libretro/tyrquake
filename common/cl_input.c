e98949c6e02fb8cc9806d04cb12aaef79089d486
libretroadmin <reallibretroretroarch@gmail.com>
2022-06-09 18:12:12 +0200

    Simplify code - get rid of unused net_win files

--- common/cl_input.c
+++ common/cl_input.c
@@ -66,9 +66,7 @@ void
 KeyDown(kbutton_t *b)
 {
     int k;
-    const char *c;
-
-    c = Cmd_Argv(1);
+    const char *c = Cmd_Argv(1);
     if (c[0])
 	k = atoi(c);
     else
@@ -95,9 +93,7 @@ void
 KeyUp(kbutton_t *b)
 {
     int k;
-    const char *c;
-
-    c = Cmd_Argv(1);
+    const char *c = Cmd_Argv(1);
     if (c[0])
 	k = atoi(c);
     else {			// typed manually at the console, assume for unsticking, so clear all
@@ -121,166 +117,139 @@ KeyUp(kbutton_t *b)
     b->state |= 4;		// impulse up
 }
 
-void
-IN_KLookDown(void)
+static void IN_KLookDown(void)
 {
     KeyDown(&in_klook);
 }
 
-void
-IN_KLookUp(void)
+static void IN_KLookUp(void)
 {
     KeyUp(&in_klook);
 }
 
-void
-IN_MLookDown(void)
+static void IN_MLookDown(void)
 {
     KeyDown(&in_mlook);
 }
 
-void
-IN_MLookUp(void)
+static void IN_MLookUp(void)
 {
     KeyUp(&in_mlook);
     if (!(in_mlook.state & 1) && lookspring.value)
 	V_StartPitchDrift();
 }
 
-void
-IN_UpDown(void)
+static void IN_UpDown(void)
 {
     KeyDown(&in_up);
 }
 
-void
-IN_UpUp(void)
+static void IN_UpUp(void)
 {
     KeyUp(&in_up);
 }
 
-void
-IN_DownDown(void)
+static void IN_DownDown(void)
 {
     KeyDown(&in_down);
 }
 
-void
-IN_DownUp(void)
+static void IN_DownUp(void)
 {
     KeyUp(&in_down);
 }
 
-void
-IN_LeftDown(void)
+static void IN_LeftDown(void)
 {
     KeyDown(&in_left);
 }
 
-void
-IN_LeftUp(void)
+static void IN_LeftUp(void)
 {
     KeyUp(&in_left);
 }
 
-void
-IN_RightDown(void)
+static void IN_RightDown(void)
 {
     KeyDown(&in_right);
 }
 
-void
-IN_RightUp(void)
+static void IN_RightUp(void)
 {
     KeyUp(&in_right);
 }
 
-void
-IN_ForwardDown(void)
+static void IN_ForwardDown(void)
 {
     KeyDown(&in_forward);
 }
 
-void
-IN_ForwardUp(void)
+static void IN_ForwardUp(void)
 {
     KeyUp(&in_forward);
 }
 
-void
-IN_BackDown(void)
+static void IN_BackDown(void)
 {
     KeyDown(&in_back);
 }
 
-void
-IN_BackUp(void)
+static void IN_BackUp(void)
 {
     KeyUp(&in_back);
 }
 
-void
-IN_LookupDown(void)
+static void IN_LookupDown(void)
 {
     KeyDown(&in_lookup);
 }
 
-void
-IN_LookupUp(void)
+static void IN_LookupUp(void)
 {
     KeyUp(&in_lookup);
 }
 
-void
-IN_LookdownDown(void)
+static void IN_LookdownDown(void)
 {
     KeyDown(&in_lookdown);
 }
 
-void
-IN_LookdownUp(void)
+static void IN_LookdownUp(void)
 {
     KeyUp(&in_lookdown);
 }
 
-void
-IN_MoveleftDown(void)
+static void IN_MoveleftDown(void)
 {
     KeyDown(&in_moveleft);
 }
 
-void
-IN_MoveleftUp(void)
+static void IN_MoveleftUp(void)
 {
     KeyUp(&in_moveleft);
 }
 
-void
-IN_MoverightDown(void)
+static void IN_MoverightDown(void)
 {
     KeyDown(&in_moveright);
 }
 
-void
-IN_MoverightUp(void)
+static void IN_MoverightUp(void)
 {
     KeyUp(&in_moveright);
 }
 
-void
-IN_SpeedDown(void)
+static void IN_SpeedDown(void)
 {
     KeyDown(&in_speed);
 }
 
-void
-IN_SpeedUp(void)
+static void IN_SpeedUp(void)
 {
     KeyUp(&in_speed);
 }
 
-void
-IN_TogglewalkDown(void)
+static void IN_TogglewalkDown(void)
 {
     KeyDown(&in_togglewalk);
     if (walkstate == 1)
@@ -289,64 +258,52 @@ IN_TogglewalkDown(void)
         walkstate = 1;
 }
 
-void
-IN_TogglewalkUp(void)
+static void IN_TogglewalkUp(void)
 {
     KeyUp(&in_togglewalk);
 }
 
-
-
-void
-IN_StrafeDown(void)
+static void IN_StrafeDown(void)
 {
     KeyDown(&in_strafe);
 }
 
-void
-IN_StrafeUp(void)
+static void IN_StrafeUp(void)
 {
     KeyUp(&in_strafe);
 }
 
-void
-IN_AttackDown(void)
+static void IN_AttackDown(void)
 {
     KeyDown(&in_attack);
 }
 
-void
-IN_AttackUp(void)
+static void IN_AttackUp(void)
 {
     KeyUp(&in_attack);
 }
 
-void
-IN_UseDown(void)
+static void IN_UseDown(void)
 {
     KeyDown(&in_use);
 }
 
-void
-IN_UseUp(void)
+static void IN_UseUp(void)
 {
     KeyUp(&in_use);
 }
 
-void
-IN_JumpDown(void)
+static void IN_JumpDown(void)
 {
     KeyDown(&in_jump);
 }
 
-void
-IN_JumpUp(void)
+static void IN_JumpUp(void)
 {
     KeyUp(&in_jump);
 }
 
-void
-IN_Impulse(void)
+static void IN_Impulse(void)
 {
     in_impulse = Q_atoi(Cmd_Argv(1));
 }
@@ -364,27 +321,18 @@ Returns 0.25 if a key was pressed and released during the frame,
 float
 CL_KeyState(kbutton_t *key)
 {
-    float val;
-    qboolean impulsedown, impulseup, down;
-
-    impulsedown = key->state & 2;
-    impulseup = key->state & 4;
-    down = key->state & 1;
-    val = 0;
+    qboolean impulsedown = key->state & 2;
+    qboolean impulseup   = key->state & 4;
+    qboolean down        = key->state & 1;
+    float val            = 0;
 
     if (impulsedown && !impulseup) {
 	if (down)
 	    val = 0.5;		// pressed and held this frame
-	else
-	    val = 0;		//      I_Error ();
     }
     // FIXME: both alternatives zero?
-    if (impulseup && !impulsedown) {
-	if (down)
-	    val = 0;		//      I_Error ();
-	else
-	    val = 0;		// released this frame
-    }
+    if (impulseup && !impulsedown)
+	    val = 0;
     if (!impulsedown && !impulseup) {
 	if (down)
 	    val = 1.0;		// held the entire frame
@@ -428,16 +376,13 @@ CL_AdjustAngles
 Moves the local angle positions
 ================
 */
-void
-CL_AdjustAngles(void)
+static void CL_AdjustAngles(void)
 {
-    float speed;
     float up, down;
+    float speed = host_frametime;
 
     if (in_speed.state & 1)
-	speed = host_frametime * cl_anglespeedkey.value;
-    else
-	speed = host_frametime;
+	speed *= cl_anglespeedkey.value;
 
     if (!(in_strafe.state & 1)) {
 	cl.viewangles[YAW] -=
@@ -515,9 +460,7 @@ CL_BaseMove(usercmd_t *cmd)
 	    cmd->forwardmove -= cl_backspeed.value * CL_KeyState(&in_back) * walkstate;
         }
     }
-//
-// adjust for speed key
-//
+    // adjust for speed key
     if (in_speed.state & 1) {
 	cmd->forwardmove *= cl_movespeedkey.value;
 	cmd->sidemove *= cl_movespeedkey.value;
