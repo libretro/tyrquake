ab99062a689d59680b4ca1933f77a464b58bf29e
U-DESKTOP-SPFP6AQ\twistedtechre <reallibretroretroarch@gmail.com>
2026-04-11 18:25:36 +0200

    See if this fixes the MSVC linker issues

--- common/shell.c
+++ common/shell.c
@@ -25,6 +25,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _MSC_VER
+#include <compat/msvc.h>
+#endif
+#include <compat/posix_string.h>
+
 #include "console.h"
 #include "cvar.h"
 #include "mathlib.h"
